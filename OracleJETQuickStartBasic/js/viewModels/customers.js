df073fe3708544bccb8c3c8f83a4a00d444e83eb
Marc Pechaitis <marcpechaitis@gmail.com>
2016-09-05 16:04:32 -0600

    feat(Customers): added a new gauge

--- OracleJETQuickStartBasic/js/viewModels/customers.js
+++ OracleJETQuickStartBasic/js/viewModels/customers.js
@@ -3,6 +3,15 @@ define(['ojs/ojcore', 'knockout', 'ojs/ojgauge'
     function customersContentViewModel() {
         var self = this;
         self.value10 = ko.observable(80);
+        self.value1 = ko.observable(88);
+        self.valueColor = ko.observable('red');
+        
+        self.gaugeOptionChange = function(e, data) {
+            if (data.option == "value") {
+              $("#gauge").attr('title', "Value: " + Math.round(data['value']) + "<br>Reference Lines: Low 33, Medium 67, High 100");
+              $("#gauge").ojStatusMeterGauge('refresh');
+            }
+        }
     }
     
     return customersContentViewModel;
