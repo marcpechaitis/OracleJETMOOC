4f38df1882b24ef85382dcbd3f86fd4cb098aba8
Marc Pechaitis <marcpechaitis@gmail.com>
2016-09-05 15:39:28 -0600

    feat(Customers): Added Gauge

--- OracleJETQuickStartBasic/js/viewModels/customers.js
+++ OracleJETQuickStartBasic/js/viewModels/customers.js
@@ -1,10 +1,9 @@
-/*
-define(['ojs/ojcore', 'knockout'
+define(['ojs/ojcore', 'knockout', 'ojs/ojgauge'
 ], function (oj, ko) {
     function customersContentViewModel() {
         var self = this;
+        self.value10 = ko.observable(80);
     }
     
     return customersContentViewModel;
 });
-*/
